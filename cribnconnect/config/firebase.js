0aa74fbce5eeb48413a905449d84ad1b85057c4f
Allen Douglas <allenissabigboi@gmail.com>
2025-08-20 10:43:44 -0700

    feat: update Firebase configuration to use environment variables

--- cribnconnect/config/firebase.js
+++ cribnconnect/config/firebase.js
@@ -10,13 +10,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB90dWTnnbAwqhs3QNnWncmnRi1PBsQaOs",
-  authDomain: "cribandconnect.firebaseapp.com",
-  projectId: "cribandconnect",
-  storageBucket: "cribandconnect.firebasestorage.app",
-  messagingSenderId: "908984266223",
-  appId: "1:908984266223:web:fdf81e12346d22cd781a1a",
-  measurementId: "G-Y6LEK89W96"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
