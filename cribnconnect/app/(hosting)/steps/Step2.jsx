99529be2a9842f7d7eafca805957689ea5719d81
Allen Douglas <allenissabigboi@gmail.com>
2025-08-23 18:34:43 -0700

    feat: update Step2 component to display space allocation options and improve user instructions

--- cribnconnect/app/(hosting)/steps/Step2.jsx
+++ cribnconnect/app/(hosting)/steps/Step2.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 
 export default function Step2({ styles }) {
+    const apartmentOption = [
+        { label: "Whole Space ", text: "Allocate the whole available space in the apartment" },
+        { label: "One Room", text: "Allocate a room or part of available space in the apartment" },
+        { label: "Shared Room", text: "Allocate a bed or space in a room of the available space" },
+    ];
   return (
     <View style={styles.stepContent}>
-      <Text style={styles.stepTitle}>Step 2</Text>
-      <Text style={styles.sectionSubtitle}>Content coming soon...</Text>
+      <Text style={styles.stepTitle}>What space are you allocating?</Text>
+      <Text style={styles.sectionSubtitle}>Select how much space is available for your listing</Text>
     </View>
   );
 }
+
