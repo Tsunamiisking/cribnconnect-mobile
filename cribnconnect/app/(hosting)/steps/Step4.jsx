be58a712858f9c1bcf74c5e57e9b824a61c26e74
Allen Douglas <allenissabigboi@gmail.com>
2025-08-23 23:27:52 -0700

    feat: update Step4 component to improve address input layout and enhance user experience

--- cribnconnect/app/(hosting)/steps/Step4.jsx
+++ cribnconnect/app/(hosting)/steps/Step4.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity, } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { useState } from "react";
 // import { KeyboardAvoidingView } from "react-native";
 
@@ -15,16 +15,36 @@ export default function Step4({ styles }) {
           Surulere, Golden tulip Festac)
         </Text>
 
-        <View className="mt-6">
+        <View className="mt-7">
           <Text style={styles.label}>Provide house address</Text>
-          <TextInput style={styles.input} placeholder="Enter address" placeholderTextColor="#B0B0B0"/>
-          <View className="flex-row"> 
-            <TextInput style={styles.inputShare} placeholder="State" placeholderTextColor="#B0B0B0"/>
-            <TextInput style={styles.inputShare} placeholder="City" placeholderTextColor="#B0B0B0"/>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter address"
+            placeholderTextColor="#B0B0B0"
+          />
+          <View className="flex-row">
+            <TextInput
+              style={styles.inputShare}
+              placeholder="State"
+              placeholderTextColor="#B0B0B0"
+            />
+            <TextInput
+              style={styles.inputShare}
+              placeholder="City"
+              placeholderTextColor="#B0B0B0"
+            />
           </View>
           <View className="flex-row">
-            <TextInput style={styles.inputShare} placeholder="Zip Code" placeholderTextColor="#B0B0B0"/>
-            <TextInput style={styles.inputShare} placeholder="Country" placeholderTextColor="#B0B0B0"/>
+            <TextInput
+              style={styles.inputShare}
+              placeholder="Zip Code"
+              placeholderTextColor="#B0B0B0"
+            />
+            <TextInput
+              style={styles.inputShare}
+              placeholder="Country"
+              placeholderTextColor="#B0B0B0"
+            />
           </View>
         </View>
       </View>
@@ -33,10 +53,37 @@ export default function Step4({ styles }) {
 
   const renderAddress = () => {
     return (
-      <View>
-        <Text style={styles.label}>
-          Please provide the address of the apartment:
-        </Text>
+      <View className="mt-7">
+        <Text style={styles.label}>Provide house address</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter address"
+          placeholderTextColor="#B0B0B0"
+        />
+        <View className="flex-row">
+          <TextInput
+            style={styles.inputShare}
+            placeholder="State"
+            placeholderTextColor="#B0B0B0"
+          />
+          <TextInput
+            style={styles.inputShare}
+            placeholder="City"
+            placeholderTextColor="#B0B0B0"
+          />
+        </View>
+        <View className="flex-row">
+          <TextInput
+            style={styles.inputShare}
+            placeholder="Zip Code"
+            placeholderTextColor="#B0B0B0"
+          />
+          <TextInput
+            style={styles.inputShare}
+            placeholder="Country"
+            placeholderTextColor="#B0B0B0"
+          />
+        </View>
       </View>
     );
   };
@@ -46,28 +93,29 @@ export default function Step4({ styles }) {
       <Text style={styles.sectionSubtitle}>Fill your location details</Text>
       <View className="mt-6">
         <Text style={styles.label}>
-          Is this apartment part of a larger serviced apartment building or complex?
+          Is this apartment part of a larger serviced apartment building or
+          complex?
         </Text>
       </View>
       <View>
         <View className="flex-row items-center">
           <TouchableOpacity
-            onPress={() => setSelected('yes')}
-            className={`w-10 h-10 border-[#274046] border-2 rounded-lg items-center justify-center mt-4 mr-4 ${selected === 'yes' ? "bg-[#274046]" : "bg-white"}`}
+            onPress={() => setSelected("yes")}
+            className={`w-10 h-10 border-[#274046] border-2 rounded-lg items-center justify-center mt-4 mr-4 ${selected === "yes" ? "bg-[#274046]" : "bg-white"}`}
           />
           <Text style={styles.typeOptionText}>Yes</Text>
         </View>
         <View className="flex-row items-center">
           <TouchableOpacity
-            onPress={() => setSelected('no')}
-            className={`w-10 h-10 border-[#274046] border-2 rounded-lg items-center justify-center mt-4 mr-4 ${selected === 'no' ? "bg-[#274046]" : "bg-white"}`}
+            onPress={() => setSelected("no")}
+            className={`w-10 h-10 border-[#274046] border-2 rounded-lg items-center justify-center mt-4 mr-4 ${selected === "no" ? "bg-[#274046]" : "bg-white"}`}
           />
           <Text style={styles.typeOptionText}>No</Text>
         </View>
       </View>
       <View>
-        {selected === 'yes' && renderComplexAddress()}
-        {selected === 'no' && renderAddress()}
+        {selected === "yes" && renderComplexAddress()}
+        {selected === "no" && renderAddress()}
       </View>
     </View>
   );
