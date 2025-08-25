a3588b399eef2c86e92ee5c5205b0abaa4a93e96
Allen Douglas <allenissabigboi@gmail.com>
2025-08-25 12:37:09 -0700

    feat: add state management for price inputs in Step7 component

--- cribnconnect/app/(hosting)/steps/Step7.jsx
+++ cribnconnect/app/(hosting)/steps/Step7.jsx
@@ -1,6 +1,9 @@
 import { View, Text, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function Step7({ styles }) {
+  const [price, setPrice] = useState();
+
   return (
     <View style={styles.stepContent}>
       <Text style={styles.stepTitle}>How much does your space cost</Text>
@@ -12,8 +15,30 @@ export default function Step7({ styles }) {
           style={styles.input}
           placeholder="Enter Price Per Night"
           placeholderTextColor="#B0B0B0"
+          keyboardType="numeric"
+          value={price}
+          onChangeText={setPrice}
         />
-        <Text>{}</Text>
+        <Text style={[styles.labelText, { marginTop: 8, marginLeft: 12 } ]}>{price}</Text>
+      </View>
+      <View style={{ marginVertical: 24, position: 'relative', justifyContent: 'center', alignItems: 'center', height: 24 }}>
+        <View style={{ position: 'absolute', left: 0, right: 0, top: '50%', height: 1, backgroundColor: '#B0B0B0' }} />
+        <View style={{ backgroundColor: '#fff', paddingHorizontal: 8 }}>
+          <Text style={[styles.labelText]}>Optional</Text>
+        </View>
+      </View>
+
+     <View>
+        <Text style={[styles.label, { marginTop: 12 }]}>Amount Per Week </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Price Per Week"
+          placeholderTextColor="#B0B0B0"
+          keyboardType="numeric"
+          value={price}
+          onChangeText={setPrice}
+        />
+        <Text style={[styles.labelText, { marginTop: 8, marginLeft: 12 } ]}>{price}</Text>
       </View>
     </View>
   );
