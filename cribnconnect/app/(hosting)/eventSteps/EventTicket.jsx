e369588ed7b559b1eac89c977ededc5312207da1
Allen Douglas <allenissabigboi@gmail.com>
2025-08-27 10:37:52 -0700

    made changes to EventTicket.jsx

--- cribnconnect/app/(hosting)/eventSteps/EventTicket.jsx
+++ cribnconnect/app/(hosting)/eventSteps/EventTicket.jsx
@@ -1,10 +1,27 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 export default function EventTicket({ styles }) {
+  const [selected, setSelected] = useState(null);
+
+  const toggleSelected = () => {
+    setSelected((prev) => (prev === "free" ? null : "free"));
+  };
   return (
     <View style={styles.stepContent}>
-      <Text style={styles.stepTitle}>Step 10</Text>
-      <Text style={styles.sectionSubtitle}>Content coming soon...</Text>
+      <Text style={styles.stepTitle}>How can people access your event?</Text>
+      <Text style={styles.sectionSubtitle}>Select Tickect Access Type</Text>
+      <View className="mt-6">
+        <View className="flex-row items-center">
+          <TouchableOpacity
+            onPress={toggleSelected}
+            className={`w-10 h-10 border-[#e5e7eb] border-2 rounded-lg items-center justify-center mt-4 mr-4 ${selected === "free" ? "bg-[#274046]" : "bg-white"}`}
+          />
+          <Text style={[styles.typeOptionText, { marginTop: 14 }]}>
+            Free ( Mark event access as FREE )
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
