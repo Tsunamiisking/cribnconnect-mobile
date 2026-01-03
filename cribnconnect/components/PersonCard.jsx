d76c444ec11bee2a8f5ee77cfaa47a7adcf2c713
Allen Douglas <allenissabigboi@gmail.com>
2026-01-03 07:41:41 -0800

    feat: add API import for chat initiation functionality in PersonCard component

--- cribnconnect/components/PersonCard.jsx
+++ cribnconnect/components/PersonCard.jsx
@@ -1,3 +1,4 @@
+import api from "@/api/api";
 import { checkExistingConversation, initiateChat } from "@/api/services/chatServices";
 import InitiateChatModal from "@/components/InitiateChatModal";
 import { Colors } from "@/constants/Colors";
